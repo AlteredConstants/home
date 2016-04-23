89f072c649310d30899a54b1fd09e54ad502557a
Zeff Svoboda <zeff@alteredconstants.com>
2016-04-22 23:00:07 -0500

    Add symlink script and use it to setup VS Code settings symlinks.

--- scripts/sudo.ps1
+++ scripts/sudo.ps1
@@ -1,18 +1,15 @@
 #!/usr/bin/env ps-exec
 
-$command, $args = $args
+Param(
+	[string]$command,
+	[parameter(ValueFromRemainingArguments = $true)]
+	[string[]]$arguments
+)
 
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
-	$runAs = "-Verb runAs"
+	Start-Process -FilePath $command -ArgumentList $arguments -Verb runAs
+} else {
+	Start-Process -FilePath $command -ArgumentList $arguments
 }
-$argList = ""
-if ($args) {
-	$firstArg, $restArgs = $args
-	$argList = "`"$firstArg`""
-	foreach ($arg in $restArgs) {
-		$argList = "$argList,`"$arg`""
-	}
-	$argList = "-ArgumentList ($argList)"
-}
-Invoke-Expression "Start-Process -FilePath `"$command`" $argList $runAs"
+
